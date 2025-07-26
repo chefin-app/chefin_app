855edab9f97a79ddd687fa6783bbc0a6e58d0990
Bavornkiet Charnpatanakorn <bill.charnpat@gmail.com>
2025-07-26 12:27:53 +0700

    Add jsx to your compilerOptions

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import { Tabs } from "expo-router";
 
 const Layout = () => {
-  return <Tabs></Tabs>;
+  return (
+    <>
+      <Tabs></Tabs>
+    </>
+  );
 };
 
 export default Layout;
