60f7623b44167d9b763f6784ea7342223f747d9b
Alex Agafonov <119372614+KuromeMochi@users.noreply.github.com>
2025-08-26 18:10:00 +0100

    useFetch Hook Impl. and More

---
+++ src/services/fetchRestaurantName.ts
@@ -0,0 +1,24 @@
+import { supabase } from "./supabase"
+import useFetch from "../hooks/useFetch"
+
+export type Restaurant = {
+  restaurant_name: string;
+};
+
+export const fetchRestaurantName = async ({ query }: { query?: string }): Promise<Restaurant[]> => {
+
+    let request = supabase.from("profiles").select("restaurant_name");
+
+    if (query && query.trim() !== "") {
+        request = request.ilike("restaurant_name", `%${query}%`); 
+    }
+
+    const { data, error } = await request;
+    //console.log("Supabase response:", { data, error });
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    return (data ?? []) as Restaurant[];
+};
